52a19daae1bbdae5394a8c8b7557c6c3b44598ff
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2023-07-10 06:44:55 +0700

    Update JumpListener.php

--- src/FiraAja/JumpRecord/JumpListener.php
+++ src/FiraAja/JumpRecord/JumpListener.php
@@ -5,3 +5,8 @@ namespace FiraAja\JumpRecord;
 use FiraAja\JumpRecord\EventListener;
 use Ifera\ScoreHud\event\PlayerTagsUpdateEvent;
 use Ifera\ScoreHud\scoreboard\ScoreTag;
+
+use pocketmine\player\Player;
+use pocketmine\event\player\PlayerJumpEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\listener\Listener;
